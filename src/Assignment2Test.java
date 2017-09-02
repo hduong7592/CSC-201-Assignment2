dd555b77370cd094ec59ce93a6ae947138179e00
Hieu Duong <hieuduong08@gmail.com>
2017-09-01 23:42:16 -0400

    Commit

---
+++ src/Assignment2Test.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Assignment2Test {
+
+    @Test
+    public void main() {
+        //Nothing to test
+    }
+
+    @org.junit.Test
+    public void getHeightValue() {
+        //Nothing to test
+    }
+
+}
